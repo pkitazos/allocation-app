469fc194e7a1d86e3b2ff528d7523dba39563b26
Petros <petroskitazos@gmail.com>
2023-09-24 23:04:50 +0100

    removed unfinished form component from admin-panel

--- src/app/(protected)/admin-panel/page.tsx
+++ src/app/(protected)/admin-panel/page.tsx
@@ -1,26 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useUser } from "@clerk/nextjs";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
-import { z } from "zod";
-
-const FormSchema = z.object({
-  type: z.enum(["all", "mentions", "none"], {
-    required_error: "You need to select a notification type.",
-  }),
-});
 
 export default function AdminPanel() {
   const createAllocationGroup = () => {
@@ -77,16 +58,14 @@ export default function AdminPanel() {
       }),
     });
   };
-
-  const handleClick = () => {};
-
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-  });
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast("changed user tier");
-  }
+  const toggleStudent = (isStudent: boolean) => {
+    fetch("/api/dev/student", {
+      method: "PATCH",
+      body: JSON.stringify({
+        isStudent,
+      }),
+    });
+  };
 
   const { user } = useUser();
 
@@ -99,7 +78,7 @@ export default function AdminPanel() {
       <div className="flex rounded-md bg-accent py-5 px-6">
         <h1 className="text-5xl text-accent-foreground">Admin Panel</h1>
       </div>
-      <Tabs defaultValue="dev-setup" className="w-[400px]">
+      <Tabs defaultValue="allocation-instance" className="w-[400px]">
         <TabsList>
           {isSuperAdmin && <TabsTrigger value="dev-setup">Dev</TabsTrigger>}{" "}
           {isSuperAdmin && (
@@ -138,77 +117,12 @@ export default function AdminPanel() {
             <Button variant="admin" onClick={createProjects}>
               add Projects
             </Button>
-            {isSuperAdmin && (
-              <>
-                <h3 className="text-destructive underline underline-offset-2 mt-6 text-xl font-medium">
-                  this is normally not here
-                </h3>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="w-2/3 space-y-6"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="type"
-                      render={({ field }) => (
-                        <FormItem className="space-y-3">
-                          <FormLabel>set user tier</FormLabel>
-                          <FormControl>
-                            <RadioGroup
-                              onValueChange={field.onChange}
-                              defaultValue={field.value}
-                              className="flex flex-col space-y-1"
-                            >
-                              <FormItem className="flex items-center space-x-3 space-y-0">
-                                <FormControl>
-                                  <RadioGroupItem value="super-admin" />
-                                </FormControl>
-                                <FormLabel className="font-normal">
-                                  super-admin
-                                </FormLabel>
-                              </FormItem>
-                              <FormItem className="flex items-center space-x-3 space-y-0">
-                                <FormControl>
-                                  <RadioGroupItem value="admin" />
-                                </FormControl>
-                                <FormLabel className="font-normal">
-                                  admin
-                                </FormLabel>
-                              </FormItem>
-                              <FormItem className="flex items-center space-x-3 space-y-0">
-                                <FormControl>
-                                  <RadioGroupItem value="supervisor" />
-                                </FormControl>
-                                <FormLabel className="font-normal">
-                                  supervisor
-                                </FormLabel>
-                              </FormItem>
-                              <FormItem className="flex items-center space-x-3 space-y-0">
-                                <FormControl>
-                                  <RadioGroupItem value="student" />
-                                </FormControl>
-                                <FormLabel className="font-normal">
-                                  student
-                                </FormLabel>
-                              </FormItem>
-                            </RadioGroup>
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <Button
-                      variant="destructive"
-                      type="submit"
-                      onClick={handleClick}
-                    >
-                      Submit
-                    </Button>
-                  </form>
-                </Form>
-              </>
-            )}
+            <Button variant="default" onClick={() => toggleStudent(true)}>
+              make me student
+            </Button>
+            <Button variant="destructive" onClick={() => toggleStudent(false)}>
+              unmake me student
+            </Button>
           </div>
         </TabsContent>
         <TabsContent value="create-admin">create admins here</TabsContent>
