73324a5a4dc4ad44b23b81f616715a40845e5536
Petros <petroskitazos@gmail.com>
2023-09-25 09:31:10 +0100

    added welcome message to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { currentUser } from "@clerk/nextjs";
+
 export default async function Home() {
-  return <>hello world</>;
+  const user = await currentUser();
+  if (!user) return;
+
+  return (
+    <div className="grid h-[92dvh] place-items-center -mt-[12dvh]">
+      <h1 className="text-4xl">
+        Welcome{" "}
+        <span className="text-secondary font-semibold">{user.username}</span>!
+      </h1>
+    </div>
+  );
 }
