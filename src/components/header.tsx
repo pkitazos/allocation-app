00127cd1e24f54a87afcca8908ce2cbf9a6aeffc
Petros <petroskitazos@gmail.com>
2023-10-05 19:30:22 +0100

    minor styling change on header and removed help page access to unauthorised users

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 "use client";
 import whiteLogo from "@/assets/uofg-white.png";
+import { Role } from "@prisma/client";
 import { User2 } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { ButtonHTMLAttributes } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import {
@@ -15,66 +17,67 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Role } from "@prisma/client";
 
-function UserButton() {
+function UserButton({ className }: ButtonHTMLAttributes<HTMLButtonElement>) {
   const { data: session } = useSession();
   const searchParams = useSearchParams();
 
   const user = session?.user;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Avatar className="cursor-pointer">
-          <AvatarImage src={user?.image ?? ""} />
-          <AvatarFallback>
-            <User2 />
-          </AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="mt-3 w-60">
-        {user && (
-          <DropdownMenuLabel className="py-4">
-            <div className="flex flex-col space-y-1 pb-2.5">
-              <p className="text-sm font-medium leading-none">{user.name}</p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {user.email}
-              </p>
-            </div>
-            <DropdownMenuSeparator />
-          </DropdownMenuLabel>
-        )}
-        <DropdownMenuItem>
-          <Link href="/account" className="w-full">
-            <Button variant="link">My account</Button>
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          {session ? (
-            <Button
-              className="w-full"
-              variant="outline"
-              onClick={async () => await signOut()}
-            >
-              Sign out
-            </Button>
-          ) : (
-            <Button
-              className="w-full"
-              variant="outline"
-              onClick={async () =>
-                await signIn("google", {
-                  callbackUrl: searchParams?.get("from") ?? "/",
-                })
-              }
-            >
-              Sign In
-            </Button>
+    <button className={className}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="cursor-pointer">
+            <AvatarImage src={user?.image ?? ""} />
+            <AvatarFallback>
+              <User2 />
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mt-3 w-60">
+          {user && (
+            <DropdownMenuLabel className="py-4">
+              <div className="flex flex-col space-y-1 pb-2.5">
+                <p className="text-sm font-medium leading-none">{user.name}</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  {user.email}
+                </p>
+              </div>
+              <DropdownMenuSeparator />
+            </DropdownMenuLabel>
           )}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem>
+            <Link href="/account" className="w-full">
+              <Button variant="link">My account</Button>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            {session ? (
+              <Button
+                className="w-full"
+                variant="outline"
+                onClick={async () => await signOut()}
+              >
+                Sign out
+              </Button>
+            ) : (
+              <Button
+                className="w-full"
+                variant="outline"
+                onClick={async () =>
+                  await signIn("google", {
+                    callbackUrl: searchParams?.get("from") ?? "/",
+                  })
+                }
+              >
+                Sign In
+              </Button>
+            )}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </button>
   );
 }
 
@@ -90,16 +93,16 @@ export function Header() {
 
   return (
     <nav className="sticky top-0 flex h-[8dvh] max-h-[5rem] w-full items-center justify-between gap-6 bg-primary px-10 py-5">
+      <Link href="/">
+        <Image
+          className="max-w-[10rem] object-scale-down"
+          width={300}
+          height={100}
+          src={whiteLogo}
+          alt="University of Glasgow logo"
+        />
+      </Link>
       <div className="flex items-center gap-6">
-        <Link href="/">
-          <Image
-            className="max-w-[10rem] object-scale-down"
-            width={300}
-            height={100}
-            src={whiteLogo}
-            alt="University of Glasgow logo"
-          />
-        </Link>
         {canAccess([
           "GROUP_ADMIN",
           "SUB_GROUP_ADMIN",
@@ -132,9 +135,6 @@ export function Header() {
             <Button variant="ghost">My Projects</Button>
           </Link>
         )}
-        <Link className="text-white hover:underline" href="/help">
-          <Button variant="ghost">Help</Button>
-        </Link>
         {canAccess(["GROUP_ADMIN", "SUB_GROUP_ADMIN"]) && (
           <>
             <Link className="text-white hover:underline" href="/students">
@@ -147,6 +147,16 @@ export function Header() {
               <Button variant="ghost">Admin Panel</Button>
             </Link>
           </>
+        )}
+        {canAccess([
+          "GROUP_ADMIN",
+          "SUB_GROUP_ADMIN",
+          "SUPERVISOR",
+          "STUDENT",
+        ]) && (
+          <Link className="text-white hover:underline" href="/help">
+            <Button variant="ghost">Help</Button>
+          </Link>
         )}
       </div>
       <UserButton />
