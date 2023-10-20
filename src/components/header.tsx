d33bd35f7aef44220cf4a51acef72c9dfcde2d61
Petros <petroskitazos@gmail.com>
2023-10-20 19:09:01 +0100

    merge changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,11 @@
 "use client";
 import whiteLogo from "@/assets/uofg-white.png";
 import { Role } from "@prisma/client";
-import { Role } from "@prisma/client";
 import { User2 } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { ButtonHTMLAttributes } from "react";
 import { ButtonHTMLAttributes } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
@@ -20,7 +18,7 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
-function UserButton({ className }: ButtonHTMLAttributes<HTMLButtonElement>) {
+
 function UserButton({ className }: ButtonHTMLAttributes<HTMLButtonElement>) {
   const { data: session } = useSession();
   const searchParams = useSearchParams();
@@ -28,59 +26,6 @@ function UserButton({ className }: ButtonHTMLAttributes<HTMLButtonElement>) {
   const user = session?.user;
 
   return (
-    <button className={className}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Avatar className="cursor-pointer">
-            <AvatarImage src={user?.image ?? ""} />
-            <AvatarFallback>
-              <User2 />
-            </AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="mt-3 w-60">
-          {user && (
-            <DropdownMenuLabel className="py-4">
-              <div className="flex flex-col space-y-1 pb-2.5">
-                <p className="text-sm font-medium leading-none">{user.name}</p>
-                <p className="text-xs leading-none text-muted-foreground">
-                  {user.email}
-                </p>
-              </div>
-              <DropdownMenuSeparator />
-            </DropdownMenuLabel>
-          )}
-          <DropdownMenuItem>
-            <Link href="/account" className="w-full">
-              <Button variant="link">My account</Button>
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            {session ? (
-              <Button
-                className="w-full"
-                variant="outline"
-                onClick={async () => await signOut()}
-              >
-                Sign out
-              </Button>
-            ) : (
-              <Button
-                className="w-full"
-                variant="outline"
-                onClick={async () =>
-                  await signIn("google", {
-                    callbackUrl: searchParams?.get("from") ?? "/",
-                  })
-                }
-              >
-                Sign In
-              </Button>
-            )}
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </button>
     <button className={className}>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
@@ -148,7 +93,6 @@ export function Header({ adminPanel }: { adminPanel: string }) {
   };
 
   return (
-    <nav className="sticky top-0 flex h-[8dvh] max-h-[5rem] w-full items-center justify-between gap-6 bg-primary px-10 py-5">
     <nav className="sticky top-0 flex h-[8dvh] max-h-[5rem] w-full items-center justify-between gap-6 bg-primary px-10 py-5">
       <Link href="/">
         <Image
@@ -225,5 +169,5 @@ export function Header({ adminPanel }: { adminPanel: string }) {
       </div>
       <UserButton />
     </nav>
-  );
+  )
 }
