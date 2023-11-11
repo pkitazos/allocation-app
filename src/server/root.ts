6b9e71b94028a0162d6bfe22a97c22b58c66612b
Petros <petroskitazos@gmail.com>
2023-11-11 12:13:08 +0000

    added student router to root

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,8 +1,10 @@
 import { projectRouter } from "./routers/project";
+import { studentRouter } from "./routers/student";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   project: projectRouter,
+  student: studentRouter,
 });
 
 export type AppRouter = typeof appRouter;
