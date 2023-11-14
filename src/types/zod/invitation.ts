da74c9d4b6a00f0d474abc6044b5f53c1fba939d
Petros <petroskitazos@gmail.com>
2023-11-14 00:50:12 +0000

    removed autogenerated files from vc

--- src/types/zod/invitation.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-import { Role } from "@prisma/client"
-
-export const InvitationModel = z.object({
-  userEmail: z.string(),
-  role: z.nativeEnum(Role),
-  signUpCode: z.string(),
-})
