855f39eb202ff79b34e7573be290ba7b8eab8688
Petros <petroskitazos@gmail.com>
2023-11-14 18:40:01 +0000

    started working on form validation

--- src/app/(protected)/[group]/[subGroup]/create-instance/form-section.tsx
+++ src/app/(protected)/[group]/[subGroup]/create-instance/form-section.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { DatePicker } from "@/components/datepicker";
 import { Input } from "@/components/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -13,12 +12,10 @@ import {
 } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
 import { api } from "@/lib/trpc/client";
-import { slugify } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { isAfter } from "date-fns";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { z } from "zod";
 
@@ -41,15 +38,22 @@ export function FormSection({
           const setOfNames = new Set(takenNames);
           return !setOfNames.has(item);
         }, "This name is already taken"),
-      minNumProjects: z.number().int().positive(),
-      maxNumProjects: z.number().int(),
-      projectUploadDeadline: z
-        .date()
-        .refine((item) => isAfter(item, new Date())),
-      minNumPreferences: z.number().int().positive(),
-      maxNumPreferences: z.number().int(),
-      maxNumPerSupervisor: z.number().int().positive(),
-      preferenceSubmissionDeadline: z.date(),
+      flags: z.array(
+        z.object({ flag: z.string().min(1, "Please add a flag") }),
+      ),
+      tags: z.array(z.object({ tag: z.string().min(1, "Please add a tag") })),
+      minNumProjects: z.coerce.number().int().positive(),
+      maxNumProjects: z.coerce.number().int().positive(),
+      // projectUploadDeadline: z
+      //   .date()
+      //   .refine(
+      //     (item) => isAfter(item, new Date()),
+      //     "Project Upload deadline can't be today",
+      //   ),
+      minNumPreferences: z.coerce.number().int().positive(),
+      maxNumPreferences: z.coerce.number().int().positive(),
+      maxNumPerSupervisor: z.coerce.number().int().positive(),
+      // preferenceSubmissionDeadline: z.date(),
     })
     .refine(
       ({ minNumProjects, maxNumProjects }) => minNumProjects <= maxNumProjects,
@@ -70,20 +74,30 @@ export function FormSection({
     )
     .refine(
       ({ maxNumPreferences, maxNumPerSupervisor }) =>
-        maxNumPreferences <= maxNumPerSupervisor,
+        maxNumPreferences >= maxNumPerSupervisor,
       {
         message:
           "Maximum Number of Preferences per supervisor can't be more than Maximum Number of Preferences",
         path: ["maxNumPerSupervisor"],
       },
     )
+    // .refine(
+    //   ({ projectUploadDeadline, preferenceSubmissionDeadline }) =>
+    //     isAfter(preferenceSubmissionDeadline, projectUploadDeadline),
+    //   {
+    //     message:
+    //       "Preference Submission deadline can't be before Project Upload deadline",
+    //     path: ["preferenceSubmissionDeadline"],
+    //   },
+    // )
     .refine(
-      ({ projectUploadDeadline, preferenceSubmissionDeadline }) =>
-        isAfter(preferenceSubmissionDeadline, projectUploadDeadline),
+      ({ flags }) => {
+        const flagSet = new Set(flags);
+        return flags.length === flagSet.size;
+      },
       {
-        message:
-          "Preference Submission deadline can't be before Project Upload deadline",
-        path: ["preferenceSubmissionDeadline"],
+        message: "Flags must have distinct values",
+        path: ["flags.0.flag"],
       },
     );
 
@@ -93,22 +107,50 @@ export function FormSection({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       instanceName: "",
+      flags: [{ flag: "" }],
+      tags: [{ tag: "" }],
+      minNumProjects: 0,
+      maxNumProjects: 0,
+      // projectUploadDeadline: new Date(),
+      minNumPreferences: 0,
+      maxNumPreferences: 0,
+      maxNumPerSupervisor: 0,
+      // preferenceSubmissionDeadline: new Date(),
     },
+  });
+  const {
+    fields: flagFields,
+    append: appendFlag,
+    remove: removeFlag,
+  } = useFieldArray({
+    control: form.control,
+    name: "flags",
+    rules: {
+      required: "Please add at least 1 Flag",
+      validate: (array) => array.length !== 0,
+    },
+  });
+  const {
+    fields: tagFields,
+    append: appendTag,
+    remove: removeTag,
+  } = useFieldArray({
+    control: form.control,
+    name: "tags",
   });
 
   const { mutateAsync: createInstanceAsync } =
     api.institution.subGroup.createInstance.useMutation();
 
-  const onSubmit = ({ instanceName }: { instanceName: string }) => {
-    console.log(instanceName);
-    void toast.promise(
+  const onSubmit = async ({ ...formData }: FormData) => {
+    console.log(formData);
+    console.log("boom");
+    await toast.promise(
       createInstanceAsync({
         groupSlug,
         subGroupSlug,
-        name: instanceName,
-      }).then(() =>
-        router.push(`/${groupSlug}/${subGroupSlug}/${slugify(instanceName)}`),
-      ),
+        name: formData.instanceName,
+      }).then(() => router.push(`/${groupSlug}/${subGroupSlug}`)),
       {
         loading: "Loading",
         error: "Something went wrong",
@@ -146,28 +188,87 @@ export function FormSection({
         </div>
         <Separator className="my-14" />
         <h3 className="text-2xl">Project Restrictions</h3>
-        <form className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
           <p>Add Project Flags</p>
-          <div className="flex gap-4">
-            <Input className="w-1/3" placeholder="Flag" />
-            <Button size="icon" type="button">
+          <div className="flex w-1/3 flex-col gap-2">
+            <Button
+              size="icon"
+              type="button"
+              onClick={() => appendFlag({ flag: "" })}
+            >
               <Plus />
             </Button>
+            {flagFields.map((item, idx) => (
+              <FormField
+                key={item.id}
+                control={form.control}
+                name="flags"
+                render={() => (
+                  <FormItem>
+                    <FormControl>
+                      <div className="flex gap-2">
+                        <Input
+                          placeholder="Flag"
+                          {...form.register(`flags.${idx}.flag`)}
+                        />
+                        <Button
+                          variant="destructive"
+                          size="icon"
+                          onClick={() => removeFlag(idx)}
+                        >
+                          <X className="h-4 w-4" />
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
           </div>
-        </form>
-        <form className="flex flex-col gap-2">
+        </div>
+        <div className="flex flex-col gap-2">
           <p>Add Project Tags</p>
-          <div className="flex gap-4">
-            <Input className="w-1/3" placeholder="Tag" />
-            <Button size="icon" type="button">
+          <div className="flex w-1/3 flex-col gap-2">
+            <Button
+              size="icon"
+              type="button"
+              onClick={() => appendTag({ tag: "" })}
+            >
               <Plus />
             </Button>
+            {tagFields.map((item, idx) => (
+              <FormField
+                key={item.id}
+                control={form.control}
+                name="tags"
+                render={() => (
+                  <FormItem>
+                    <FormControl>
+                      <div className="flex gap-2">
+                        <Input
+                          placeholder="Tag"
+                          {...form.register(`tags.${idx}.tag`)}
+                        />
+                        <Button
+                          variant="destructive"
+                          size="icon"
+                          onClick={() => removeTag(idx)}
+                        >
+                          <X className="h-4 w-4" />
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
           </div>
-        </form>
+        </div>
         <Separator className="my-14" />
         <h3 className="text-2xl">Supervisor Restrictions</h3>
-        <form className="grid w-1/2 grid-cols-2 gap-5">
-          {/* TODO: handle submit on debounce */}
+        <div className="grid w-1/2 grid-cols-2 gap-5">
           <p className="place-self-end self-center">
             Minimum number of Projects:
           </p>
@@ -206,7 +307,7 @@ export function FormSection({
               </FormItem>
             )}
           />
-          <p className="place-self-end self-center">Project upload deadline:</p>
+          {/* <p className="place-self-end self-center">Project upload deadline:</p>
           <FormField
             control={form.control}
             name="projectUploadDeadline"
@@ -218,11 +319,11 @@ export function FormSection({
                 <FormMessage />
               </FormItem>
             )}
-          />
-        </form>
+          /> */}
+        </div>
         <Separator className="my-14" />
         <h3 className="text-2xl">Student Restrictions</h3>
-        <form className="grid w-1/2 grid-cols-2 gap-5">
+        <div className="grid w-1/2 grid-cols-2 gap-5">
           <p className="place-self-end self-center">
             Minimum number of Preferences:
           </p>
@@ -278,7 +379,7 @@ export function FormSection({
               </FormItem>
             )}
           />
-          <p className="place-self-end self-center">
+          {/* <p className="place-self-end self-center">
             Preference submission deadline:
           </p>
           <FormField
@@ -292,11 +393,15 @@ export function FormSection({
                 <FormMessage />
               </FormItem>
             )}
-          />
-        </form>
+          /> */}
+        </div>
         <Separator className="my-10" />
         <div className="flex justify-end">
-          <Button type="submit" size="lg">
+          <Button
+            type="submit"
+            size="lg"
+            onClick={() => console.log(form.formState.errors)}
+          >
             create new instance
           </Button>
         </div>
