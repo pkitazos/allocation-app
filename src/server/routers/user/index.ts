e787d386863f1ff490f8fa360569a243f9067805
Petros <petroskitazos@gmail.com>
2024-01-07 23:48:28 +0000

    trpc procedure to get all user allocation instances

--- src/server/routers/user/index.ts
+++ src/server/routers/user/index.ts
@@ -1,4 +1,8 @@
-import { createTRPCRouter, publicProcedure } from "@/server/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/trpc";
 import { z } from "zod";
 import { studentRouter } from "./student";
 import { supervisorRouter } from "./supervisor";
@@ -51,5 +55,107 @@ export const userRouter = createTRPCRouter({
     }
 
     return "";
+  }),
+
+  instances: protectedProcedure.query(async ({ ctx }) => {
+    const { id, role } = ctx.session.user;
+
+    if (!role || role === "UNREGISTERED") return [];
+
+    if (role === "SUPERVISOR") {
+      return await ctx.db.supervisorInInstance.findMany({
+        where: {
+          supervisorId: id,
+        },
+        select: {
+          allocationGroupId: true,
+          allocationSubGroupId: true,
+          allocationInstanceId: true,
+        },
+      });
+    }
+
+    if (role === "STUDENT") {
+      return await ctx.db.studentInInstance.findMany({
+        where: {
+          studentId: id,
+        },
+        select: {
+          allocationGroupId: true,
+          allocationSubGroupId: true,
+          allocationInstanceId: true,
+        },
+      });
+    }
+
+    if (role === "SUB_GROUP_ADMIN") {
+      const data = await ctx.db.subGroupAdmin.findFirstOrThrow({
+        where: { id },
+        select: {
+          allocationSubGroup: {
+            select: {
+              allocationInstances: {
+                select: {
+                  allocationGroupId: true,
+                  allocationSubGroupId: true,
+                  slug: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      return data.allocationSubGroup.allocationInstances.map((item) => ({
+        allocationGroupId: item.allocationGroupId,
+        allocationSubGroupId: item.allocationSubGroupId,
+        allocationInstanceId: item.slug,
+      }));
+    }
+
+    if (role === "GROUP_ADMIN") {
+      const data = await ctx.db.groupAdmin.findFirstOrThrow({
+        where: { id },
+        select: {
+          allocationGroup: {
+            select: {
+              allocationSubGroups: {
+                select: {
+                  allocationInstances: {
+                    select: {
+                      allocationGroupId: true,
+                      allocationSubGroupId: true,
+                      slug: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+
+      return data.allocationGroup.allocationSubGroups
+        .flatMap((item) => item.allocationInstances)
+        .map((item) => ({
+          allocationGroupId: item.allocationGroupId,
+          allocationSubGroupId: item.allocationSubGroupId,
+          allocationInstanceId: item.slug,
+        }));
+    }
+
+    const instances = await ctx.db.allocationInstance.findMany({
+      select: {
+        allocationGroupId: true,
+        allocationSubGroupId: true,
+        slug: true,
+      },
+    });
+
+    return instances.map((item) => ({
+      allocationGroupId: item.allocationGroupId,
+      allocationSubGroupId: item.allocationSubGroupId,
+      allocationInstanceId: item.slug,
+    }));
   }),
 });
