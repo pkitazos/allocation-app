a5b7c0adde4570e5faa92626bb1ed87c344bc478
Petros <petroskitazos@gmail.com>
2024-01-09 10:50:18 +0000

    minor ui fixes

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -8,11 +8,12 @@ export default async function Page() {
   const { superAdmin, groups } = await api.institution.groupManagement.query();
 
   return (
-    <div className="mt-6 flex flex-col gap-10 px-6 pb-20">
+    <div className="mt-6 flex w-full max-w-5xl flex-col gap-10 px-6 pb-20">
       <h1 className="text-4xl">University of Glasgow</h1>
-      <div className="my-10 flex flex-col gap-2 rounded-md bg-accent/50 px-5 pb-7 pt-5">
+
+      <div className="my-10 flex min-h-40 flex-col gap-2 rounded-md bg-accent/50 px-5 pb-7 pt-5">
         <h3 className="mb-4 text-2xl underline">Super-Admins</h3>
-        <div className="flex items-center gap-5">
+        <div className="flex h-9 items-center gap-5">
           <div className="w-1/6 font-medium">{superAdmin.name}</div>
           <Separator orientation="vertical" />
           <div className="w-/4">{superAdmin.email}</div>
