b8e038493aeb400f8d15d76436f2223af844fc9e
Petros <93052465+pkitazos@users.noreply.github.com>
2024-01-21 11:55:54 +0000

    made basic ui and data loading for user-instances component

--- src/components/user-instances.tsx
+++ src/components/user-instances.tsx
@@ -1,3 +1,20 @@
-export function UserInstances() {
-  return <></>;
+import { api } from "@/lib/trpc/server";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export async function UserInstances() {
+  const instances = await api.user.instances.query();
+
+  return (
+    <div className="h-40">
+      <h2 className="mb-10 text-xl font-semibold underline decoration-secondary decoration-4">
+        Your instances
+      </h2>
+      {instances.map((instance, i) => (
+        <Button className="h-24 w-48" key={i} variant="outline" size="lg">
+          <Link href="">{instance.allocationInstanceId}</Link>
+        </Button>
+      ))}
+    </div>
+  );
 }
