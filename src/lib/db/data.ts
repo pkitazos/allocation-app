545d28e43f87e9b66bf3140ad3ad664e95ee83a7
Petros <93052465+pkitazos@users.noreply.github.com>
2024-01-22 23:00:47 +0000

    major schema refactor

--- src/lib/db/data.ts
+++ src/lib/db/data.ts
@@ -87,136 +87,50 @@ export const projectData = [
     description:
       "Real-time weather forecasts and predictive weather alerts for any location",
   },
-  // {
-  //   title: "Home Automation System",
-  //   description:
-  //     "Controls home appliances, lights, temperature through a central smart home platform",
-  // },
-  // {
-  //   title: "Fitness Tracker",
-  //   description: "Tracks daily steps, activity, sleep and other health metrics",
-  // },
-  // {
-  //   title: "Task Management App",
-  //   description:
-  //     "Kanban boards, reminders and collaborative features to manage projects and tasks",
-  // },
-  // {
-  //   title: "Video Streaming Service",
-  //   description:
-  //     "On-demand video streaming with recommendations and social features",
-  // },
-  // {
-  //   title: "Travel Expense Tracker",
-  //   description:
-  //     "Tracks travel expenses, generates reports and reimbursable amounts",
-  // },
-  // {
-  //   title: "Online Multiplayer Game",
-  //   description:
-  //     "Competitive or cooperative gameplay in an online persistent world",
-  // },
-  // {
-  //   title: "Restaurant Booking Platform",
-  //   description:
-  //     "Table reservations, customer management and payment processing for restaurants",
-  // },
-  // {
-  //   title: "Feedback Collection System",
-  //   description:
-  //     "Surveys, interviews and feedback analytics for product development",
-  // },
-  // {
-  //   title: "Online Learning Platform",
-  //   description: "Online courses, resources and tools for remote learning",
-  // },
-  // {
-  //   title: "Email Marketing Campaigns",
-  //   description:
-  //     "Create, schedule and track email campaigns and analyze engagement",
-  // },
-  // {
-  //   title: "Translation Service",
-  //   description: "Translates text and documents between multiple languages",
-  // },
-  // {
-  //   title: "Conference Management System",
-  //   description:
-  //     "Manage conference registrations, schedules, payments and attendees",
-  // },
-  // {
-  //   title: "Sales Pipeline CRM",
-  //   description:
-  //     "Organize leads, deals and client interactions for sales teams",
-  // },
-  // {
-  //   title: "Online Code Editor",
-  //   description:
-  //     "Browser-based code editor with instant previews, sharing and collaboration",
-  // },
-  // {
-  //   title: "Data Visualization Platform",
-  //   description: "Interactive data visualizations, dashboards and analytics",
-  // },
-  // {
-  //   title: "2D Game Engine",
-  //   description: "A framework for building 2D games across multiple platforms",
-  // },
-  // {
-  //   title: "Automated Testing Framework",
-  //   description: "Tools to automate testing of software systems and interfaces",
-  // },
-  // {
-  //   title: "Invoice Management System",
-  //   description: "Generate, send and track invoices and accept online payments",
-  // },
-  // {
-  //   title: "Password Manager App",
-  //   description: "Securely generate, store and fill passwords across devices",
-  // },
-  // {
-  //   title: "Team Collaboration Platform",
-  //   description:
-  //     "Tools for communication, file sharing, task management and collaboration",
-  // },
-  // {
-  //   title: "SEO Optimization Tools",
-  //   description: "Tools to optimize websites for search engine discoverability",
-  // },
-  // {
-  //   title: "Social Media Analytics",
-  //   description:
-  //     "Insights into social media campaigns, competitors and industry benchmarking",
-  // },
-  // {
-  //   title: "Real Estate Listings Platform",
-  //   description: "Search, showcase and manage real estate listings and sales",
-  // },
-  // {
-  //   title: "Cryptocurrency Exchange",
-  //   description: "Trade digital currencies and manage crypto wallets",
-  // },
-  // {
-  //   title: "Bug and Issue Tracker",
-  //   description:
-  //     "Track software bugs, tasks and feature requests throughout development",
-  // },
 ];
 
 export const invitationData = [
-  { userEmail: "super.allocationapp@gmail.com", role: Role.SUPER_ADMIN },
-  { userEmail: "group.allocationapp@gmail.com", role: Role.GROUP_ADMIN },
-  { userEmail: "subgroup.allocationapp@gmail.com", role: Role.SUB_GROUP_ADMIN },
+  { userEmail: "super.allocationapp@gmail.com", role: Role.ADMIN },
+  { userEmail: "group.allocationapp@gmail.com", role: Role.ADMIN },
+  { userEmail: "subgroup.allocationapp@gmail.com", role: Role.ADMIN },
   { userEmail: "supervisor.allocationapp@gmail.com", role: Role.SUPERVISOR },
   { userEmail: "student.allocationapp@gmail.com", role: Role.STUDENT },
 ];
 
+export const testSuperAdmin = {
+  email: "super.allocationapp@gmail.com",
+};
+
+export const testGroup = {
+  id: "school-of-computing-science",
+  displayName: "School of Computing Science",
+};
+
+export const testGroupAdmin = {
+  email: "group.allocationapp@gmail.com",
+};
+
+export const testSubGroup = {
+  id: "level-4-individual-project",
+  displayName: "Level 4 Individual Project",
+};
+
+export const testSubGroupAdmin = {
+  email: "subgroup.allocationapp@gmail.com",
+};
+
+export const testInstance = {
+  id: "2023",
+  displayName: "2023",
+};
+
+export const testStudent = {
+  id: "clrpi3qoc000cwuazkpjyj102", // TODO: grab from prisma
+  name: "Eva",
+  email: "student.allocationapp@gmail.com",
+};
+
 export const studentData = [
-  {
-    id: "2345678e",
-    name: "Eva",
-    email: "student.allocationapp@gmail.com",
-  },
   {
     id: "2345678f",
     name: "Frank",
@@ -249,12 +163,13 @@ export const studentData = [
   },
 ];
 
+export const testSupervisor = {
+  id: "12345d",
+  name: "Dan",
+  email: "supervisor.allocationapp@gmail.com",
+};
+
 export const supervisorData = [
-  {
-    id: "12345d",
-    name: "Dan",
-    email: "supervisor.allocationapp@gmail.com",
-  },
   {
     id: "12345e",
     name: "Ellen",
@@ -269,7 +184,7 @@ export const supervisorData = [
 
 export const supervisorInInstanceData = [
   {
-    id: "12345d",
+    id: "clrpi3ly90009wuaz4tw9wq0m", // TODO: grab from prisma
     projectAllocationTarget: 2,
     projectAllocationUpperBound: 3,
   },
--- src/lib/db/seed.ts
+++ src/lib/db/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { AdminLevel, PrismaClient, Role } from "@prisma/client";
 import {
   allAlgorithms,
   flagData,
@@ -9,159 +9,289 @@ import {
   supervisorData,
   supervisorInInstanceData,
   tagData,
+  testGroup,
+  testGroupAdmin,
+  testInstance,
+  testStudent,
+  testSubGroup,
+  testSubGroupAdmin,
+  testSuperAdmin,
+  testSupervisor,
 } from "./data";
 
 const db = new PrismaClient();
 
-async function main() {
-  const debug = false;
+function dbg<T>(label: string, obj?: T) {
+  const mode = true;
+  if (mode) console.log(`------------ ${label}`, obj ?? "");
+}
 
-  console.log("SEEDING");
-  const superAdmin = await db.superAdmin.create({
+async function main() {
+  console.log("---------->> SEEDING");
+
+  // await db.user.create({
+  //   data: {
+  //     id: "super-admin",
+  //     name: "SUPER_ADMIN",
+  //     email: testSuperAdmin.email,
+  //   },
+  // });
+  // await db.user.create({
+  //   data: {
+  //     name: "GROUP_ADMIN",
+  //     email: testGroupAdmin.email,
+  //   },
+  // });
+  // await db.user.create({
+  //   data: {
+  //     name: "SUB_GROUP_ADMIN",
+  //     email: testSubGroupAdmin.email,
+  //   },
+  // });
+  // await db.user.create({
+  //   data: {
+  //     id: testSupervisor.id,
+  //     name: "SUPERVISOR",
+  //     email: testSupervisor.email,
+  //   },
+  // });
+  // await db.user.create({
+  //   data: {
+  //     id: testStudent.id,
+  //     name: "STUDENT",
+  //     email: testStudent.email,
+  //   },
+  // });
+
+  const superAdmin = await db.adminInSpace.create({
     data: {
-      name: "Alice",
-      email: "super.allocationapp@gmail.com",
+      role: AdminLevel.SUPER,
+      user: { connect: { email: testSuperAdmin.email } },
     },
   });
-  if (debug) console.log("------------ SUPER-ADMIN", superAdmin);
+  dbg("SUPER-ADMIN", superAdmin);
+
+  const groupAdmin = await db.adminInSpace.create({
+    data: {
+      role: AdminLevel.SUB_GROUP,
+      user: { connect: { email: testGroupAdmin.email } },
+    },
+  });
+  dbg("GROUP ADMIN", groupAdmin);
+
+  const subGroupAdmin = await db.adminInSpace.create({
+    data: {
+      role: AdminLevel.SUB_GROUP,
+      user: { connect: { email: testSubGroupAdmin.email } },
+    },
+  });
+  dbg("SUB_GROUP ADMIN", subGroupAdmin);
 
   const allocationGroup = await db.allocationGroup.create({
     data: {
-      slug: "school-of-computing-science",
-      displayName: "School of Computing Science",
-      superAdminId: superAdmin.id,
+      id: testGroup.id,
+      displayName: testGroup.displayName,
+      groupAdmins: { connect: { systemId: groupAdmin.systemId } },
     },
   });
-  if (debug) console.log("------------ ALLOCATION GROUP", allocationGroup);
-
-  const groupAdmin = await db.groupAdmin.create({
-    data: {
-      name: "Bob",
-      email: "group.allocationapp@gmail.com",
-      allocationGroupId: allocationGroup.slug,
-    },
-  });
-  if (debug) console.log("------------ GROUP ADMIN", groupAdmin);
+  dbg("GROUP", allocationGroup);
 
   const allocationSubGroup = await db.allocationSubGroup.create({
     data: {
-      slug: "level-4-individual-project",
-      displayName: "Level 4 Individual Project",
-      allocationGroupId: allocationGroup.slug,
+      allocationGroupId: allocationGroup.id,
+      id: testSubGroup.id,
+      displayName: testSubGroup.displayName,
+      subGroupAdmins: { connect: { systemId: subGroupAdmin.systemId } },
     },
   });
-  if (debug)
-    console.log("------------ ALLOCATION SUB GROUP", allocationSubGroup);
-
-  const subGroupAdmin = await db.subGroupAdmin.create({
-    data: {
-      name: "Chris",
-      email: "subgroup.allocationapp@gmail.com",
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
-    },
-  });
-  if (debug) console.log("------------ SUB GROUP ADMIN", subGroupAdmin);
+  dbg("SUB_GROUP", allocationSubGroup);
 
   const allocationInstance = await db.allocationInstance.create({
     data: {
-      slug: "2023",
-      displayName: "2023",
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      id: testInstance.id,
+      displayName: testInstance.displayName,
     },
   });
-  if (debug)
-    console.log("------------ ALLOCATION INSTANCE", allocationInstance);
+  dbg("INSTANCE", allocationInstance);
 
-  const supervisor = await db.supervisor
-    .createMany({
-      data: supervisorData,
-    })
-    .then(async () => await db.supervisor.findMany({}));
-  if (debug) console.log("------------ SUPERVISOR", supervisor);
+  const supervisor = await db.userInInstance.create({
+    data: {
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
+      role: Role.SUPERVISOR,
+      user: { connect: { email: testSupervisor.email } },
+    },
+  });
+  dbg("SUPERVISOR", supervisor);
 
-  await db.supervisorInInstance.createMany({
-    data: supervisorInInstanceData.map((item) => ({
-      supervisorId: item.id,
-      projectAllocationLowerBound: 0,
-      projectAllocationTarget: item.projectAllocationTarget,
-      projectAllocationUpperBound: item.projectAllocationUpperBound,
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
-      allocationInstanceId: allocationInstance.slug,
+  await db.user.createMany({
+    data: supervisorData.map(({ id, name, email }) => ({
+      id,
+      name,
+      email,
     })),
   });
+  dbg("SUPERVISORS - USERS");
+
+  const [supervisor2, supervisor3] = await db.userInInstance
+    .createMany({
+      data: supervisorData.map(({ id }) => ({
+        allocationGroupId: allocationGroup.id,
+        allocationSubGroupId: allocationSubGroup.id,
+        allocationInstanceId: allocationInstance.id,
+        role: Role.SUPERVISOR,
+        userId: id, // ? this now hopefully works
+      })),
+    })
+    .then(
+      async () =>
+        await db.userInInstance.findMany({
+          where: { role: Role.SUPERVISOR, NOT: { userId: supervisor.userId } },
+          take: 2, // ? not actually helpful
+        }),
+    );
+  dbg("SUPERVISORS - REST", { supervisor, supervisor2, supervisor3 });
+
+  for (const item of supervisorInInstanceData) {
+    await db.supervisorInstanceDetails.create({
+      data: {
+        projectAllocationLowerBound: 0,
+        projectAllocationTarget: item.projectAllocationTarget,
+        projectAllocationUpperBound: item.projectAllocationUpperBound,
+        userInInstance: {
+          connect: {
+            userId_allocationGroupId_allocationSubGroupId_allocationInstanceId:
+              {
+                allocationGroupId: allocationGroup.id,
+                allocationSubGroupId: allocationSubGroup.id,
+                allocationInstanceId: allocationInstance.id,
+                userId: item.id,
+              },
+          },
+        },
+      },
+    });
+  }
+  dbg("SUPERVISORS - DETAILS");
 
   const flags = await db.flag
     .createMany({ data: flagData })
     .then(async () => await db.flag.findMany({}));
+  dbg("FLAGS");
 
   const tags = await db.tag
     .createMany({ data: tagData })
     .then(async () => await db.tag.findMany({}));
+  dbg("TAGS");
 
-  const students = await db.student
-    .createMany({ data: studentData })
-    .then(async () => await db.student.findMany({ orderBy: { id: "asc" } }));
+  const student = await db.userInInstance.create({
+    data: {
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
+      role: Role.STUDENT,
+      user: { connect: { email: testStudent.email } },
+    },
+  });
+  dbg("STUDENT", student);
 
-  await db.studentInInstance.createMany({
-    data: students.map(({ id: studentId }) => ({
-      studentId,
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
-      allocationInstanceId: allocationInstance.slug,
+  await db.user.createMany({
+    data: studentData.map(({ id, name, email }) => ({
+      id,
+      name,
+      email,
     })),
   });
+  dbg("STUDENTS - USERS");
+
+  const students = await db.userInInstance
+    .createMany({
+      data: studentData.map(({ id }) => ({
+        allocationGroupId: allocationGroup.id,
+        allocationSubGroupId: allocationSubGroup.id,
+        allocationInstanceId: allocationInstance.id,
+        role: Role.STUDENT,
+        userId: id, // ? this now hopefully works
+      })),
+    })
+    .then(
+      async () =>
+        await db.userInInstance.findMany({
+          where: { role: Role.STUDENT },
+          orderBy: { userId: "asc" },
+        }),
+    );
+  dbg("STUDENTS - REST");
+
+  await db.studentInstanceDetails.createMany({
+    data: students.map(({ userId }) => ({
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
+      userId,
+    })),
+  });
+  dbg("STUDENTS - DETAILS");
 
   await db.flagOnStudent.createMany({
-    data: students.map(({ id: studentId }) => ({
-      studentId,
+    data: students.map(({ userId }) => ({
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
+      userId,
+
       flagId: flags[0].id,
     })),
   });
+  dbg("STUDENTS - FLAGS");
 
   await db.project.createMany({
     data: projectData.slice(0, 3).map(({ id, title, description }) => ({
       id,
       title,
       description,
-      supervisorId: supervisor[0].id,
+      supervisorId: supervisor.userId,
       capacityLowerBound: 0,
       capacityUpperBound: 1,
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
-      allocationInstanceId: allocationInstance.slug,
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
     })),
   });
+  dbg("PROJECTS - SUPERVISOR 1");
 
   await db.project.createMany({
     data: projectData.slice(3, 6).map(({ id, title, description }) => ({
       id,
       title,
       description,
-      supervisorId: supervisor[1].id,
+      supervisorId: supervisor2.userId,
       capacityLowerBound: 0,
       capacityUpperBound: 1,
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
-      allocationInstanceId: allocationInstance.slug,
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
     })),
   });
+  dbg("PROJECTS - SUPERVISOR 2");
 
   await db.project.createMany({
     data: projectData.slice(6, 8).map(({ id, title, description }) => ({
       id,
       title,
       description,
-      supervisorId: supervisor[2].id,
+      supervisorId: supervisor3.userId,
       capacityLowerBound: 0,
       capacityUpperBound: 1,
-      allocationGroupId: allocationGroup.slug,
-      allocationSubGroupId: allocationSubGroup.slug,
-      allocationInstanceId: allocationInstance.slug,
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
     })),
   });
+  dbg("PROJECTS - SUPERVISOR 3");
 
   const projects = await db.project.findMany({});
 
@@ -171,6 +301,7 @@ async function main() {
       flagId: flags[0].id,
     })),
   });
+  dbg("PROJECTS - FLAGS");
 
   await db.tagOnProject.createMany({
     data: projects.map(({ id: projectId }) => ({
@@ -178,13 +309,14 @@ async function main() {
       tagId: tags[0].id,
     })),
   });
+  dbg("PROJECTS - TAGS");
 
   await db.algorithm.createMany({
     data: allAlgorithms.map(
       ({ algName, displayName, description, flag1, flag2, flag3 }) => ({
-        allocationGroupId: allocationGroup.slug,
-        allocationSubGroupId: allocationSubGroup.slug,
-        allocationInstanceId: allocationInstance.slug,
+        allocationGroupId: allocationGroup.id,
+        allocationSubGroupId: allocationSubGroup.id,
+        allocationInstanceId: allocationInstance.id,
         algName,
         displayName,
         description,
@@ -195,22 +327,27 @@ async function main() {
       }),
     ),
   });
+  dbg("ALGORITHMS");
 
   await db.preference.createMany({
     data: preferenceData.map(({ idx, projectId, rank, type }) => ({
-      studentId: students[idx].id,
+      allocationGroupId: allocationGroup.id,
+      allocationSubGroupId: allocationSubGroup.id,
+      allocationInstanceId: allocationInstance.id,
+      userId: students[idx].userId,
       projectId,
       rank,
       type,
     })),
   });
+  dbg("PREFERENCES");
 
   await db.invitation.createMany({
     data: invitationData,
   });
 
   console.log("ok");
-  console.log("SEEDING COMPLETE");
+  console.log("<<---------- SEEDING COMPLETE");
 }
 
 main()
