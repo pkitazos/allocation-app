a7a22ede3e27c34ca0e58b55337ff48da687f4e8
Petros <93052465+pkitazos@users.noreply.github.com>
2024-02-12 18:19:53 +0000

    started building form

--- src/app/(protected)/[group]/[subGroup]/[instance]/(supervisor)/my-projects/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(supervisor)/my-projects/page.tsx
@@ -3,14 +3,32 @@ import { Role } from "@prisma/client";
 import { Unauthorised } from "@/components/unauthorised";
 import { api } from "@/lib/trpc/server";
 import { instanceParams } from "@/lib/validations/params";
+import { Heading } from "@/components/heading";
+import { Card } from "@/components/ui/card";
 
 export default async function Page({ params }: { params: instanceParams }) {
   const role = await api.user.role.query({ params });
 
-  if (role === Role.SUPERVISOR) {
+  if (role !== Role.SUPERVISOR) {
     return (
       <Unauthorised message="You need to be a Supervisor to access this page" />
     );
   }
-  return <div>this is your projects page page</div>;
+
+  const { projects, targets } = await api.user.supervisor.projects.query({
+    params,
+  });
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <Heading title="My Projects" />
+      <Card className="flex justify-between px-10 py-5">
+        <h2>targets</h2>
+        <p>{targets}</p>
+      </Card>
+      {projects.map((e) => (
+        <p key={e.id}>{e.title}</p>
+      ))}
+    </div>
+  );
 }
