50526b3ea18763c511522b039cc7025003c93a93
Petros <93052465+pkitazos@users.noreply.github.com>
2024-02-20 12:54:53 +0000

    removed submission area after submission period closes

--- src/app/(protected)/[group]/[subGroup]/[instance]/(student)/my-preferences/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(student)/my-preferences/page.tsx
@@ -1,10 +1,11 @@
-import { Role } from "@prisma/client";
+import { Role, Stage } from "@prisma/client";
 
 import { Heading } from "@/components/heading";
 import { PanelWrapper } from "@/components/panel-wrapper";
 import { Unauthorised } from "@/components/unauthorised";
 
 import { api } from "@/lib/trpc/server";
+import { stageCheck } from "@/lib/utils/permissions/stage-check";
 import { InstanceParams } from "@/lib/validations/params";
 
 import { KanbanBoard } from "./_components/kanban-board";
@@ -18,6 +19,7 @@ export default async function Page({ params }: { params: InstanceParams }) {
       <Unauthorised message="You need to be a Student to access this page" />
     );
   }
+  const stage = await api.institution.instance.currentStage.query({ params });
 
   const { initialColumns, initialProjects } =
     await api.user.student.preference.initialBoardState.query({ params });
@@ -30,7 +32,9 @@ export default async function Page({ params }: { params: InstanceParams }) {
     <>
       <Heading>My Preferences</Heading>
       <PanelWrapper className="mt-10 h-full">
-        <SubmissionButton restrictions={restrictions} />
+        {!stageCheck(stage, Stage.PROJECT_ALLOCATION) && (
+          <SubmissionButton restrictions={restrictions} />
+        )}
         <div className="flex w-full max-w-7xl flex-col">
           <KanbanBoard
             initialColumns={initialColumns}
