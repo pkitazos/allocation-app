df7ddca9b81f31fc7a999ea0f214b922d3a6207a
Petros <93052465+pkitazos@users.noreply.github.com>
2024-02-29 21:52:17 +0000

    added updated algorithm descriptions

--- src/lib/algorithms/index.ts
+++ src/lib/algorithms/index.ts
@@ -2,12 +2,11 @@ import { AlgorithmFlag } from "@prisma/client";
 
 import { Algorithm } from "@/lib/validations/algorithm";
 
-// TODO: add actual descriptions
 export const GenerousAlgorithm = {
   algName: "generous",
   displayName: "Generous",
   description:
-    "Aims to to create the fairest distribution of matches across all students, ensuring that even the least desirable projects are as fairly allocated as possible before considering a student's higher preference.",
+    "Produces a matching that has maximum cardinality, and subject to this, maximises the number of first choices, and subject to this, maximises the number of second choices, etc.",
   flag1: AlgorithmFlag.MAXSIZE,
   flag2: AlgorithmFlag.GEN,
   flag3: AlgorithmFlag.LSB,
@@ -17,7 +16,7 @@ export const GreedyAlgorithm = {
   algName: "greedy",
   displayName: "Greedy",
   description:
-    "Aims to to optimise the allocation so that the maximum number of students are matched with their higher-preferred choices, even if it compromises the number of students getting their lower-preferred choices.",
+    "Produces a matching that has maximum cardinality, and subject to this, minimises the number of Rth choices, and subject to this, minimises the number of (R-1)th choices, etc., where R is the maximum length of a preference list.",
   flag1: AlgorithmFlag.MAXSIZE,
   flag2: AlgorithmFlag.GRE,
   flag3: AlgorithmFlag.LSB,
@@ -26,7 +25,8 @@ export const GreedyAlgorithm = {
 export const MinCostAlgorithm = {
   algName: "minimum-cost",
   displayName: "Minimum Cost",
-  description: "description",
+  description:
+    "Produces a maximum cardinality matching that has minimum cost, where the cost of a matching is the sum, taken over all matched students, of the rank of each student's assigned project in their preference list.",
   flag1: AlgorithmFlag.MAXSIZE,
   flag2: AlgorithmFlag.MINCOST,
   flag3: AlgorithmFlag.LSB,
@@ -36,7 +36,7 @@ export const GreedyGenAlgorithm = {
   algName: "greedy-generous",
   displayName: "Greedy-Generous",
   description:
-    "Starts by satisfying students' highest preferences as much as possible, and once the lowest successful match is identified, it redistributes the remaining opportunities in a way that is as fair as possible to all students.",
+    "Produces a greedy maximum matching relative to the first k elements on every student's preference list, where k is the maximum integer such that some agent obtains their kth choice project in a generous maximum matching.",
   flag1: AlgorithmFlag.MAXSIZE,
   flag2: AlgorithmFlag.GRE,
   flag3: AlgorithmFlag.LSB,
