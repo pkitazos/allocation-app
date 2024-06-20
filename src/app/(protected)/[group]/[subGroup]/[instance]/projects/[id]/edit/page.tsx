44ba7d6eecddbf4f6ae75eb02b87bdfb259c8cdb
Petros <93052465+pkitazos@users.noreply.github.com>
2024-06-20 15:18:58 +0100

    Added access control
    Added access control checks to edit project page

--- src/app/(protected)/[group]/[subGroup]/[instance]/projects/[id]/edit/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/projects/[id]/edit/page.tsx
@@ -5,18 +5,35 @@ import { api } from "@/lib/trpc/server";
 import { InstanceParams } from "@/lib/validations/params";
 
 import { EditProjectForm } from "./_components/edit-project-form";
+import { auth } from "@/lib/auth";
+import { Role, Stage } from "@prisma/client";
+import { stageCheck } from "@/lib/utils/permissions/stage-check";
+import { Unauthorised } from "@/components/unauthorised";
 
 type PageParams = InstanceParams & { id: string };
 
 export default async function Page({ params }: { params: PageParams }) {
-  //   TODO: check that user trying to access this is an admin or the project supervisor
-
   const projectId = params.id;
+
+  const user = await api.user.get();
+  const role = await api.user.role({ params });
+  const stage = await api.institution.instance.currentStage({ params });
+
+  const project = await api.project.getById({ projectId });
+
+  if (role !== Role.ADMIN && user.id !== project.supervisor.id) {
+    return (
+      <Unauthorised message="You need to be an Admin to access this page" />
+    );
+  }
+  if (stageCheck(stage, Stage.PROJECT_ALLOCATION)) {
+    return (
+      <Unauthorised message="You are not allowed to access the platform at this time" />
+    );
+  }
 
   const { flags, tags, students } =
     await api.institution.instance.project.creationDetails({ params });
-
-  const project = await api.project.getById({ projectId });
 
   const { capacityUpperBound, preAllocatedStudentId } =
     await api.project.getEditFormDetails({ projectId });
