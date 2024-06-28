a446ec0e654f17ae8fe70e4bed0eeeefe50254a3
Petros <93052465+pkitazos@users.noreply.github.com>
2024-06-28 11:31:46 +0100

    Modified Access Control for supervisors pages.
    Only Admins can view the supervisors and supervisors/[id] pages
    
    Co-authored-by: AlysonDick <AlysonDick@users.noreply.github.com>

--- src/app/(protected)/[group]/[subGroup]/[instance]/supervisors/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/supervisors/page.tsx
@@ -1,8 +1,5 @@
-import { Role } from "@prisma/client";
-
 import { Heading } from "@/components/heading";
 import { PageWrapper } from "@/components/page-wrapper";
-import { Unauthorised } from "@/components/unauthorised";
 
 import { api } from "@/lib/trpc/server";
 import { InstanceParams } from "@/lib/validations/params";
@@ -12,9 +9,6 @@ import { SupervisorsDataTable } from "./_components/supervisors-data-table";
 export default async function Page({ params }: { params: InstanceParams }) {
   const { user, role } = await api.user.userRole({ params });
 
-  if (role !== Role.ADMIN && role !== Role.SUPERVISOR) {
-    return <Unauthorised message="You don't have access to this page" />;
-  }
   const stage = await api.institution.instance.currentStage({ params });
   const tableData = await api.institution.instance.supervisors({
     params,
