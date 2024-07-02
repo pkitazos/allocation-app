51cc7be1253d1aa3cc160ede33db24328923a5ec
Petros <93052465+pkitazos@users.noreply.github.com>
2024-07-02 15:09:53 +0100

    Added comments to stage check functions
    - JSDoc-style comments will hopefully make maintenance easier in the future

--- src/lib/utils/permissions/stage-check.ts
+++ src/lib/utils/permissions/stage-check.ts
@@ -3,26 +3,60 @@ import { Stage } from "@prisma/client";
 import { stageOrd } from "@/lib/db";
 import { stageSchema } from "@/lib/validations/stage";
 
+/**
+ *
+ * @param currentStage the current stage
+ * @param minStage the minimum stage to compare against
+ * @returns true if the current stage is greater than the minimum stage
+ */
 export function stageGt(currentStage: Stage, minStage: Stage) {
   return stageOrd[currentStage] > stageOrd[minStage];
 }
 
+/**
+ *
+ * @param currentStage the current stage
+ * @param minStage the minimum stage to compare against
+ * @returns true if the current stage is greater than or equal to the minimum stage
+ */
 export function stageGte(currentStage: Stage, minStage: Stage) {
   return stageOrd[currentStage] >= stageOrd[minStage];
 }
 
-export function stageLt(currentStage: Stage, minStage: Stage) {
-  return stageOrd[currentStage] < stageOrd[minStage];
+/**
+ *
+ * @param currentStage the current stage
+ * @param maxStage the maximum stage to compare against
+ * @returns true if the current stage is less than the maximum stage
+ */
+export function stageLt(currentStage: Stage, maxStage: Stage) {
+  return stageOrd[currentStage] < stageOrd[maxStage];
 }
 
-export function stageLte(currentStage: Stage, minStage: Stage) {
-  return stageOrd[currentStage] <= stageOrd[minStage];
+/**
+ *
+ * @param currentStage the current stage
+ * @param maxStage the maximum stage to compare against
+ * @returns true if the current stage is less than or equal to the maximum stage
+ */
+export function stageLte(currentStage: Stage, maxStage: Stage) {
+  return stageOrd[currentStage] <= stageOrd[maxStage];
 }
 
+/**
+ *
+ * @param minStage the minimum stage to include in the returned array
+ * @returns all stages that are after the `minStage` inclusive
+ */
 export function subsequentStages(minStage: Stage) {
   return stageSchema.options.filter((s) => stageOrd[s] >= stageOrd[minStage]);
 }
 
+/**
+ *
+ * @param maxStage the maximum stage to include in the returned array
+ * @returns all stages that are before the `maxStage` inclusive
+ */
 export function previousStages(maxStage: Stage) {
   return stageSchema.options.filter((s) => stageOrd[s] <= stageOrd[maxStage]);
 }
