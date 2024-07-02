552f57d3ada4694fd61fbc56e5fa20879ade6c51
Petros <93052465+pkitazos@users.noreply.github.com>
2024-07-02 14:11:48 +0100

    removed Admin level check from AccessControl component
    - Admin Level check was incorrectly added to this component, leaving it as a separate check improves code reusability

--- src/components/access-control/index.tsx
+++ src/components/access-control/index.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { ReactNode } from "react";
 import { AdminLevel, Role, Stage } from "@prisma/client";
+import { ReactNode } from "react";
 
 import { stageSchema } from "@/lib/validations/stage";
 
-import { AdminLevelAC } from "./admin-level-ac";
 import { RBAC } from "./rbac";
 import { SBAC } from "./sbac";
 
@@ -12,13 +11,11 @@ export function AccessControl({
   children,
   allowedStages = stageSchema.options,
   allowedRoles = [Role.ADMIN, Role.SUPERVISOR, Role.STUDENT],
-  minimumAdminLevel,
   extraConditions,
 }: {
   children: ReactNode;
   allowedStages?: Stage[];
   allowedRoles?: Role[];
-  minimumAdminLevel?: AdminLevel;
   extraConditions?: {
     RBAC?: { AND?: boolean; OR?: boolean };
     SBAC?: { AND?: boolean; OR?: boolean };
@@ -35,9 +32,7 @@ export function AccessControl({
         AND={extraConditions?.SBAC?.AND}
         OR={extraConditions?.SBAC?.OR}
       >
-        <AdminLevelAC minimumAdminLevel={minimumAdminLevel}>
-          {children}
-        </AdminLevelAC>
+        {children}
       </SBAC>
     </RBAC>
   );
