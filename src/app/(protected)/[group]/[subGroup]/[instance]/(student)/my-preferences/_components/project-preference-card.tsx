3b010dd29f0cfc57bd989f50355fc0c46dcee3ab
Petros <93052465+pkitazos@users.noreply.github.com>
2024-07-08 09:37:56 +0100

    Updated imports

--- src/app/(protected)/[group]/[subGroup]/[instance]/(student)/my-preferences/_components/project-preference-card.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(student)/my-preferences/_components/project-preference-card.tsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { cn } from "@/lib/utils";
-import { previousStages, stageGte } from "@/lib/utils/permissions/stage-check";
+import { stageGte } from "@/lib/utils/permissions/stage-check";
 import { ProjectPreference } from "@/lib/validations/board";
 
 export function ProjectPreferenceCard({
