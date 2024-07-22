9e0eff031d793bbe66fd0e17cd5073f71c4ab9c5
Petros <93052465+pkitazos@users.noreply.github.com>
2024-07-22 12:40:46 +0100

    Fixed send button styling

--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-6)/export-data/_components/send-data-section.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-6)/export-data/_components/send-data-section.tsx
@@ -3,11 +3,18 @@
 import { Button } from "@/components/ui/button";
 
 import { AllocationCsvData } from "@/lib/validations/allocation-csv-data";
+import { ShareIcon } from "lucide-react";
 
 export function SendDataSection({ data }: { data: AllocationCsvData[] }) {
   return (
     <div>
-      <Button>Send</Button>
+      <Button
+        variant="secondary"
+        className="flex cursor-pointer items-center gap-2"
+      >
+        <ShareIcon className="h-4 w-4" />
+        <p>Send</p>
+      </Button>
     </div>
   );
 }
