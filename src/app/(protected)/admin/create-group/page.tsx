7dc175ec9f602edd83c541840b060fc9843b047f
AlysonDick <alysondick1995@gmail.com>
2024-07-31 14:52:43 +0100

    Renamed takenNames to takenGroupNames for clarity
    Variable name takenNames is used throughout project referring to different names e.g. instance names, group names etc.

--- src/app/(protected)/admin/create-group/page.tsx
+++ src/app/(protected)/admin/create-group/page.tsx
@@ -1,11 +1,11 @@
 import { api } from "@/lib/trpc/server";
 
-import { spacesLabels } from "@/content/spaces";
-
 import { FormSection } from "./_components/form-section";
 
+import { spacesLabels } from "@/content/spaces";
+
 export default async function Page() {
-  const takenNames = await api.institution.takenGroupNames();
+  const takenGroupNames = await api.institution.takenGroupNames();
   return (
     <div className="mt-6 flex flex-col gap-10 px-6">
       <h2 className="text-4xl">
@@ -14,7 +14,7 @@ export default async function Page() {
           {spacesLabels.group.full}
         </span>
       </h2>
-      <FormSection takenNames={takenNames} />
+      <FormSection takenGroupNames={takenGroupNames} />
     </div>
   );
 }
