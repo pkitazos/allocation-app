d0a5b12596106db24b73eda94bfbdbf1b22a3662
Petros <93052465+pkitazos@users.noreply.github.com>
2024-08-08 21:20:21 +0100

    Fixed new student columns
    - changed column ID to match column name
    - removed redundant handlers
    - fixed imports
    - centred checkboxes in selection column

--- src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-1)/add-students/_components/new-student-columns.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/(admin-panel)/(stage-1)/add-students/_components/new-student-columns.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { Stage } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal as MoreIcon, Trash2Icon } from "lucide-react";
 
-import { useInstanceStage } from "@/components/params-context";
 import { Button } from "@/components/ui/button";
 import { ActionColumnLabel } from "@/components/ui/data-table/action-column-label";
 import { DataTableColumnHeader } from "@/components/ui/data-table/data-table-column-header";
@@ -31,7 +29,7 @@ export function constructColumns({
 
   const userCols: ColumnDef<NewStudent>[] = [
     {
-      id: "full Name",
+      id: "Full Name",
       accessorFn: ({ fullName }) => fullName,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Full Name" />
@@ -50,7 +48,7 @@ export function constructColumns({
       ),
     },
     {
-      id: "guid",
+      id: "GUID",
       accessorFn: ({ institutionId: guid }) => guid,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="GUID" canFilter />
@@ -66,14 +64,14 @@ export function constructColumns({
       ),
     },
     {
-      id: "email",
+      id: "Email",
       accessorFn: ({ email }) => email,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Email" />
       ),
     },
     {
-      id: "level",
+      id: "Student Level",
       accessorFn: ({ level }) => level,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Student Level" />
@@ -95,10 +93,6 @@ export function constructColumns({
           .getSelectedRowModel()
           .rows.map((e) => e.original.institutionId);
 
-        function handleRemoveStudents() {
-          void removeSelectedStudents(selectedStudentIds);
-        }
-
         if (someSelected)
           return (
             <div className="flex w-14 items-center justify-center">
@@ -115,7 +109,9 @@ export function constructColumns({
                   <DropdownMenuItem className="text-destructive focus:bg-red-100/40 focus:text-destructive">
                     <button
                       className="flex items-center gap-2"
-                      onClick={handleRemoveStudents}
+                      onClick={async () =>
+                        void removeSelectedStudents(selectedStudentIds)
+                      }
                     >
                       <Trash2Icon className="h-4 w-4" />
                       <span>Remove Selected Students</span>
@@ -132,36 +128,31 @@ export function constructColumns({
         row: {
           original: { fullName, institutionId },
         },
-      }) => {
-        function handleRemoveStudent() {
-          void removeStudent(institutionId);
-        }
-        return (
-          <div className="flex w-14 items-center justify-center">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size="icon" variant="ghost">
-                  <span className="sr-only">Open menu</span>
-                  <MoreIcon className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="center" side="bottom">
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="text-destructive focus:bg-red-100/40 focus:text-destructive">
-                  <button
-                    className="flex items-center gap-2"
-                    onClick={handleRemoveStudent}
-                  >
-                    <Trash2Icon className="h-4 w-4" />
-                    <span>Remove Student {fullName}</span>
-                  </button>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        );
-      },
+      }) => (
+        <div className="flex w-14 items-center justify-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <span className="sr-only">Open menu</span>
+                <MoreIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center" side="bottom">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="text-destructive focus:bg-red-100/40 focus:text-destructive">
+                <button
+                  className="flex items-center gap-2"
+                  onClick={async () => void removeStudent(institutionId)}
+                >
+                  <Trash2Icon className="h-4 w-4" />
+                  <span>Remove Student {fullName}</span>
+                </button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      ),
     },
   ];
 
