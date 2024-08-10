72c8a96286a9d3894942c638a7a4824e54404e92
Petros <93052465+pkitazos@users.noreply.github.com>
2024-08-10 19:34:45 +0100

    minor fix in labels

--- src/app/(protected)/[group]/_components/admin-removal-button.tsx
+++ src/app/(protected)/[group]/_components/admin-removal-button.tsx
@@ -1,14 +1,14 @@
 "use client";
-
 import { Trash2Icon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
+import { YesNoAction } from "@/components/yes-no-action";
 
 import { api } from "@/lib/trpc/client";
 import { GroupParams } from "@/lib/validations/params";
-import { YesNoAction } from "@/components/yes-no-action";
+import { spacesLabels } from "@/content/spaces";
 
 export function AdminRemovalButton({
   params,
@@ -21,27 +21,22 @@ export function AdminRemovalButton({
 
   const { mutateAsync } = api.institution.group.removeAdmin.useMutation();
 
-  //   const utils = api.useUtils();
-  //   const refetch = () =>
-  //     utils.institution.group.subGroupManagement.refetch({ params });
-
   function handleRemoval() {
     void toast.promise(
       mutateAsync({ params, userId }).then(() => router.refresh()),
       {
-        loading: "Removing Group Admin",
+        loading: `Removing ${spacesLabels.group.short} admin...`,
         error: "Something went wrong",
-        success: "Success",
+        success: `Successfully removed ${spacesLabels.group.short} admin`,
       },
     );
   }
 
-  // TODO maybe requires a better message?
   return (
     <YesNoAction
       action={handleRemoval}
-      title="Remove Group Admin?"
-      description="You are about to remove a group admin. Do you wish to proceed?"
+      title={`Remove ${spacesLabels.group.short} Admin?`}
+      description={`You are about to remove a ${spacesLabels.group.short} admin. Do you wish to proceed?`}
       trigger={
         <Button variant="destructive" className="flex items-center gap-2">
           <Trash2Icon className="h-4 w-4" />
