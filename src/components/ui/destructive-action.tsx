551aa9ed10704bb5de993b1eeef5cf4cac010c9a
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-08-10 13:30:51 +0100

    created destructive-action component

---
+++ src/components/ui/destructive-action.tsx
@@ -0,0 +1,125 @@
+"use client";
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
+import React, {
+  createContext,
+  Dispatch,
+  forwardRef,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog";
+import { Input, InputProps } from "./input";
+
+const DestructiveActionContext = createContext<{
+  action: () => void;
+  requiresVerification: boolean;
+  verified: boolean;
+  setVerified: Dispatch<SetStateAction<boolean>>;
+}>({
+  action: () => {},
+  requiresVerification: false,
+  verified: false,
+  setVerified: () => {},
+});
+
+const DestructiveAction = ({
+  action,
+  requiresVerification = false,
+  ...rest
+}: AlertDialogPrimitive.AlertDialogProps & {
+  action: () => void;
+  requiresVerification?: boolean;
+}) => {
+  const [verified, setVerified] = useState(!requiresVerification);
+
+  return (
+    <DestructiveActionContext.Provider
+      value={{ action, requiresVerification, verified, setVerified }}
+    >
+      <AlertDialog {...rest} />
+    </DestructiveActionContext.Provider>
+  );
+};
+
+const DestructiveActionConfirm = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Action>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
+>(({ ...props }, ref) => {
+  const { action, requiresVerification, verified } = useContext(
+    DestructiveActionContext,
+  );
+
+  return (
+    <AlertDialogPrimitive.Action
+      ref={ref}
+      {...props}
+      onClick={action}
+      disabled={requiresVerification && !verified}
+    />
+  );
+});
+DestructiveActionConfirm.displayName = "DestructiveActionConfirm";
+
+const DestructiveActionVerificationTypeIn = forwardRef<
+  HTMLInputElement,
+  Omit<InputProps, "onChange" | "value"> & { phrase: string }
+>(({ phrase, ...rest }) => {
+  const [state, setState] = useState("");
+
+  const { setVerified } = useContext(DestructiveActionContext);
+
+  useEffect(() => {
+    if (state === phrase) setVerified(true);
+    else setVerified(false);
+  }, [phrase, setVerified, state]);
+
+  return (
+    <Input
+      {...rest}
+      value={state}
+      placeholder={phrase}
+      onChange={(e) => setState(e.target.value)}
+    />
+  );
+});
+DestructiveActionVerificationTypeIn.displayName =
+  "DestructiveActionVerificationTypeIn";
+
+const DestructiveActionCancel = AlertDialogCancel;
+const DestructiveActionTrigger = AlertDialogTrigger;
+const DestructiveActionContent = AlertDialogContent;
+const DestructiveActionHeader = AlertDialogHeader;
+const DestructiveActionTitle = AlertDialogTitle;
+const DestructiveActionDescription = AlertDialogDescription;
+const DestructiveActionFooter = AlertDialogFooter;
+const DestructiveActionOverlay = AlertDialogOverlay;
+const DestructiveActionPortal = AlertDialogPortal;
+
+export {
+  DestructiveAction,
+  DestructiveActionCancel,
+  DestructiveActionConfirm,
+  DestructiveActionContent,
+  DestructiveActionDescription,
+  DestructiveActionFooter,
+  DestructiveActionHeader,
+  DestructiveActionOverlay,
+  DestructiveActionPortal,
+  DestructiveActionTitle,
+  DestructiveActionTrigger,
+  DestructiveActionVerificationTypeIn,
+};
