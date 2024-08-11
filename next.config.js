617f48f809b887b232612a1b08650a4bf28cd4e5
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-08-11 13:12:31 +0100

    added bundle analyzer to try and triage large packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /*eslint-env node*/
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = require("@next/bundle-analyzer")({ enabled: false })({
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  output: "standalone",
-};
+  // output: "standalone", // this line will cause errors on windows
+});
 
 module.exports = nextConfig;
