a01387f0f56a525abb0ddb1c132e3e6c37563618
Petros <93052465+pkitazos@users.noreply.github.com>
2024-08-12 20:23:53 +0100

    Removed project flag sorting from table headers

--- src/app/(protected)/[group]/[subGroup]/[instance]/projects/_components/all-projects-columns.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/projects/_components/all-projects-columns.tsx
@@ -104,9 +104,7 @@ export function useAllProjectsColumns({
     {
       id: "Flags",
       accessorFn: (row) => row.flags,
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Flags" />
-      ),
+      header: () => <div className="text-center">Flags</div>,
       filterFn: (row, columnId, value) => {
         const ids = value as string[];
         const rowFlags = row.getValue(columnId) as TagType[];
