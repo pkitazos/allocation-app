881ec61836fce1615194bcc65fbe516803c13a97
Petros <93052465+pkitazos@users.noreply.github.com>
2024-08-13 17:25:27 +0100

    updated seed script

--- src/lib/scripts/seed.ts
+++ src/lib/scripts/seed.ts
@@ -1,20 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 import { superAdmin_levels, superAdmin_users } from "@/lib/db/data";
-import { dbg } from "@/lib/utils/general/console-debug";
 
 const db = new PrismaClient();
 
 async function main() {
-  console.log("------------>> SEEDING\n");
-
   await db.$transaction(async (tx) => {
     await tx.user.createMany({ data: superAdmin_users });
     await tx.adminInSpace.createMany({ data: superAdmin_levels });
-    dbg("super-admins\n");
   });
 
-  console.log("<<------------ SEEDING COMPLETE");
+  console.log("SEEDING COMPLETE");
 }
 
 main()
