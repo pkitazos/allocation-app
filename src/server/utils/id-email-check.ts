a3bbef59a2a01ed42da90ab8634b6cd1ea5f1b2e
William Pettersson <william.pettersson@glasgow.ac.uk>
2024-08-16 12:44:02 +0100

    Update email address if old is blank
    If a user has an empty email address, and someone adds them as a
    supervisor but with an email address, update their email address.
    
    Closes: #2

--- src/server/utils/id-email-check.ts
+++ src/server/utils/id-email-check.ts
@@ -8,23 +8,42 @@ export async function validateEmailGUIDMatch(
   email: string,
   name: string,
 ) {
+  // Find user with this email address
   let user = await tx.user.findFirst({
     where: { id: institutionId, email },
   });
 
-  if (!user) {
-    try {
-      user = await tx.user.create({
-        data: {
-          id: institutionId,
-          name,
-          email,
-        },
-      });
-    } catch (e) {
-      // this email is already taken
-      throw new TRPCClientError("GUID and email do not match");
-    }
+  if (user) {
+    return user;
+  }
+  // See if this user exists with no/empty email address
+  user = await tx.user.findFirst({
+    where: { id: institutionId, email: "" },
+  });
+  if (user) {
+    // We found the user with a blank email, but the function
+    // parameter email isn't empty (else the earlier return would
+    // fire) so update the email address
+    user = await tx.user.update({
+      where: {id: institutionId},
+      data: {
+        email: email
+      },
+    });
+    return user;
+  }
+  // Could not find the user, so create them
+  try {
+    user = await tx.user.create({
+      data: {
+        id: institutionId,
+        name,
+        email,
+      },
+    });
+  } catch (e) {
+    // this email is already taken
+    throw new TRPCClientError("GUID and email do not match");
   }
 
   return user;
