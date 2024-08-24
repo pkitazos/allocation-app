1726e888e8d8c3f10a4d2e686f330bb07f1c245b
Petros <93052465+pkitazos@users.noreply.github.com>
2024-08-24 17:17:42 +0100

    Added metadata to supervisor new-project page

--- src/app/(protected)/[group]/[subGroup]/[instance]/supervisors/[id]/new-project/page.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/supervisors/[id]/new-project/page.tsx
@@ -6,9 +6,25 @@ import { api } from "@/lib/trpc/server";
 import { makeRequiredFlags } from "@/lib/utils/general/make-required-flags";
 import { InstanceParams } from "@/lib/validations/params";
 
+import { app, metadataTitle } from "@/content/config/app";
 import { pages } from "@/content/pages";
 
 type PageParams = InstanceParams & { id: string };
+
+export async function generateMetadata({ params }: { params: PageParams }) {
+  const { displayName } = await api.institution.instance.get({ params });
+  const { name } = await api.user.getById({ userId: params.id });
+
+  return {
+    title: metadataTitle([
+      pages.newProject.title,
+      name,
+      pages.allSupervisors.title,
+      displayName,
+      app.name,
+    ]),
+  };
+}
 
 export default async function Page({ params }: { params: PageParams }) {
   const supervisor = await api.user.getById({ userId: params.id });
