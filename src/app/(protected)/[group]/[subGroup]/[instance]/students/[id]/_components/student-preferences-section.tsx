23b27519f80c882926a44d767c90db39c156a189
Petros <93052465+pkitazos@users.noreply.github.com>
2024-08-26 10:54:40 +0100

    [WIP] started feature which allows admins to add projects to student preference lists

--- src/app/(protected)/[group]/[subGroup]/[instance]/students/[id]/_components/student-preferences-section.tsx
+++ src/app/(protected)/[group]/[subGroup]/[instance]/students/[id]/_components/student-preferences-section.tsx
@@ -1,9 +1,13 @@
+import { BookmarkPlusIcon } from "lucide-react";
+
 import { SubHeading } from "@/components/heading";
 import { StudentSavedPreferenceDataTable } from "@/components/student-saved-preferences/data-table";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { api } from "@/lib/trpc/server";
+import { cn } from "@/lib/utils";
 import { PageParams } from "@/lib/validations/params";
 
 import { StudentPreferenceDataTable } from "./student-preference-data-table";
@@ -27,7 +31,19 @@ export async function StudentPreferencesSection({
 
   return (
     <>
-      <SubHeading>Preferences</SubHeading>
+      <div className="-mb-2 mt-6 flex items-center justify-between">
+        <SubHeading>Preferences</SubHeading>
+        <Button
+          className={cn(
+            buttonVariants({ variant: "secondary" }),
+            "flex items-center justify-center gap-2 text-nowrap",
+          )}
+        >
+          <BookmarkPlusIcon className="h-4 w-4" />
+          <p>Add Project to Preferences</p>
+        </Button>
+      </div>
+
       <Tabs defaultValue="current-board-state" className="w-full">
         <TabsList className="w-full">
           <TabsTrigger
